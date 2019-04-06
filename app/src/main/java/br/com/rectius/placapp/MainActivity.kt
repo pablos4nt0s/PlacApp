455e34a81191ab7daf51d46726312ea2ce5b0e1b
Pablo Souza <pablodss@gmail.com>
2019-04-06 15:52:48 -0300

    Initial commit

---
+++ app/src/main/java/br/com/rectius/placapp/MainActivity.kt
@@ -0,0 +1,28 @@
+package br.com.rectius.placapp
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import br.com.rectius.placapp.extensions.value
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btStart.setOnClickListener{
+            val nextScreenIntent = Intent(this,
+                GameActivity::class.java)
+
+            nextScreenIntent.putExtra("HOME",
+                inputHome.value())
+
+            nextScreenIntent.putExtra("AWAY",
+                inputAway.value())
+
+            startActivity(nextScreenIntent)
+        }
+    }
+}
